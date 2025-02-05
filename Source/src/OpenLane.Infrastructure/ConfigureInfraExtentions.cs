9c15117ec5e2b1e8983c313db063f51fdef315a6
jochens <jochen.schoonjans@remmicom.be>
2025-02-05 20:40:09 +0100

    Update nuget packages

--- Source/src/OpenLane.Infrastructure/ConfigureInfraExtentions.cs
+++ Source/src/OpenLane.Infrastructure/ConfigureInfraExtentions.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OpenLane.Domain.Services;
-using OpenLane.Infrastructure.Factories;
 using OpenLane.Infrastructure.Services;
 
 namespace OpenLane.Infrastructure;
@@ -16,25 +15,13 @@ public static class ConfigureInfraExtentions
 			options.UseSqlServer(configuration.GetConnectionString("AppDB"));
 		});
 
-		services.AddDistributedCache(configuration);
-
-		services.AddTransient<IIdempotencyService, IdempotencyService>();
-		services.AddTransient<ILockService, LockService>();
-
-		return services;
-	}
-
-	private static IServiceCollection AddDistributedCache(this IServiceCollection services, IConfiguration configuration)
-	{
-		// This complex setup is done, so AddRedisInstrumentation will produce trace logging.
-
-		services.AddSingleton(sp => ConnectionMultiplexerFactory.CreateInstance(configuration));
-
 		services.AddStackExchangeRedisCache(options =>
 		{
 			options.Configuration = configuration.GetConnectionString("DistributedCache")!;
-			options.ConnectionMultiplexerFactory = () => Task.FromResult(ConnectionMultiplexerFactory.CreateInstance(configuration));
 		});
+
+		services.AddTransient<IIdempotencyService, IdempotencyService>();
+		services.AddTransient<ILockService, LockService>();
 
 		return services;
 	}
